a0e6b085f42c6ddc43dad794c59eda761ca30dd9
ayisha92farishta <ayisha_farishta@yahoo.com>
2021-08-20 20:00:19 +0000

    Made the function return false, and check words vertically

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,30 @@
 const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+   
+    //searching words horizontally
+     const horizontalJoin = letters.map(ls => ls.join(''))
+     for (l of horizontalJoin) {
+         if (l.includes(word)) return true;
+     }
+     
+ 
+    //transpose the letetrs
+    const lettersTranspose = transpose(letters);
+ 
+    //searching word hirizontally over the newly transposed letters
+ 
+    const verticalJoin = lettersTranspose.map(ls => ls.join(''))
+    for (v of verticalJoin) {
+     if (v.includes(word)) return true;
     }
-}
-
-module.exports = wordSearch
+ 
+   return false;
+ 
+ };
+ 
+ //function to transpose 
+ const transpose = function(letters) {
+     return letters[0].map((x, colIndex) => letters.map(row => row[colIndex]));
+ }
+ 
+ 
+ module.exports = wordSearch
